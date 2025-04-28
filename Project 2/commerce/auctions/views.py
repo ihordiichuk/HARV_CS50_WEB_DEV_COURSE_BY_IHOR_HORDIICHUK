b69f373405241c98c10334383c4339f5413b3660
Ihor Hordiichuk <lordwayden@gmail.com>
2025-04-28 21:47:36 +0300

    Display all active listings on the homepage

--- Project 2/commerce/auctions/views.py
+++ Project 2/commerce/auctions/views.py
@@ -1,16 +1,23 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import User
+from .models import Listing
 from .forms import ListingForm
 
+
+
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all()
+    
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
