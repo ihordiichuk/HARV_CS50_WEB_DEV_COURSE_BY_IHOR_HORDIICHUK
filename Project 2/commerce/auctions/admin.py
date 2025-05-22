a16263f252cbdb99bcef439444b8d69868570367
Ihor Hordiichuk <lordwayden@gmail.com>
2025-05-22 22:02:22 +0300

    Register all models in Django admin with English comments
    - User, Listing, Bid models registered
    - Ready for admin panel access

--- Project 2/commerce/auctions/admin.py
+++ Project 2/commerce/auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Listing, Bid, User
 
-# Register your models here.
+# Register User model to appear in the Django admin panel
+admin.site.register(User)
+
+# Register Listing model so listings can be managed via admin interface
+admin.site.register(Listing)
+
+# Register Bid model to view and manage bids in admin
+admin.site.register(Bid)
