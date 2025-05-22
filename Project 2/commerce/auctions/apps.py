18e5277d11523d13807be644530f49daebf85a87
Ihor Hordiichuk <lordwayden@gmail.com>
2025-05-22 22:03:50 +0300

    Add English comments to apps.py
    - Documented default_auto_field
    - Clarified app name registration

--- Project 2/commerce/auctions/apps.py
+++ Project 2/commerce/auctions/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class AuctionsConfig(AppConfig):
+    # Default primary key field type for models
+    default_auto_field = 'django.db.models.BigAutoField'
+
+    # Name of the Django app
     name = 'auctions'
