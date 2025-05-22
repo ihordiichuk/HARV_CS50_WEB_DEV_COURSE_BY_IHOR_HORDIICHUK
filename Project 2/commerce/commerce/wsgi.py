846a7291dd838ef9850ef3dfbf4827243775ef19
Ihor Hordiichuk <lordwayden@gmail.com>
2025-05-22 22:13:16 +0300

    Add English comments to wsgi.py
    - Described WSGI setup for deployment
    - Ensured correct settings module is loaded

--- Project 2/commerce/commerce/wsgi.py
+++ Project 2/commerce/commerce/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set default settings module for WSGI-compatible web servers
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
 
+# Get the WSGI application callable for deployment
 application = get_wsgi_application()
